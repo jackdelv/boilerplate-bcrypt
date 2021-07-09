0209ef24de333f7bdfbc1dac5459903b42dd4a9a
jackdelv <jackdelv39@gmail.com>
2021-07-09 12:15:48 -0400

    Added features to helmet object

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -10,24 +11,24 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+  if (err) return console.log(err);
+  console.log(hash);
+  
+  bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+  if (err) return console.log(err);
+  console.log(res);
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
-
-
-
-
-
-
-
-
 
 
 
